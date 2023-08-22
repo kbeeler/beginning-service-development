4e3c2e666d19df3e362e5f859ad45376c470d641
Kevin Beeler <kbeeler07@gmail.com>
2023-08-22 16:32:43 -0400

    end of day push 8/22

---
+++ containers/Demo2/greet.sh
@@ -0,0 +1,2 @@
+echo "Hello again from the container running ubuntu linux"
+cat /usr/lib/os-release
---
+++ containers/Demo3/src/hello.c
@@ -0,0 +1,21 @@
+[12:28 PM] Jeff Gonzalez
+
+#include <stdio.h>
+
+#include <time.h>
+
+ 
+
+int main(void) {
+
+    time_t t;
+
+    time(&t);
+
+ 
+
+    printf("I'm running in a container Running some freaking C code OMG! It is now %s\n", ctime(&t));
+
+    return (0);
+
+}
